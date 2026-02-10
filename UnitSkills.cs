e95e395e81a922660ff928f24376f6bd7919113a
Jocelyn <jocelyn2brunel@gmail.com>
2026-02-10 14:31:05 -0500

    Add files via upload

--- UnitSkills.cs
+++ UnitSkills.cs
@@ -1,10 +1,28 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace XCOM_3
 {
-    public enum SkillType { Endurance, Marksmanship, Strength, Tactics, Willpower }
+    public enum SkillType
+    {
+        Endurance,
+        Marksmanship,
+        Strength,
+        Tactics,
+        Willpower,
+
+        // Nouveaux
+        Perception,
+        Intelligence,
+        Stealth,
+        Medical,
+        Demolitions,
+        Leadership,
+        Resilience,
+        WeaponHandling,
+        Mobility
+    }
 
     public sealed class SkillProgress
     {
@@ -52,7 +70,8 @@ namespace XCOM_3
         }
 
         // XP Gain
-        public void GainMovementXP(int distance) => _skills[SkillType.Endurance].AddXP(distance * 5);
+        public void GainMovementXP(int distance) => 
+            _skills[SkillType.Endurance].AddXP(distance * 5);
 
         public void GainShootingXP(bool hit, int distance, int damage)
         {
@@ -62,10 +81,15 @@ namespace XCOM_3
         public void GainGrenadeXP(int enemiesHit, int totalDamage) =>
             _skills[SkillType.Strength].AddXP(10 + enemiesHit * 8 + totalDamage / 3);
 
-        public void GainCoverXP() => _skills[SkillType.Tactics].AddXP(8);
+
+        // Meilleure couverture avec le peu de cover auquel on a accès.
+        public void GainCoverXP() => 
+            _skills[SkillType.Tactics].AddXP(8);
+
 
         public void GainSurvivalXP(int damageTaken, bool survived) =>
             _skills[SkillType.Willpower].AddXP(damageTaken / 5 + (survived ? 15 : 0));
+
 
         public void GainKillXP(string enemyClass)
         {
@@ -73,12 +97,101 @@ namespace XCOM_3
             _skills[SkillType.Willpower].AddXP(10);
         }
 
+        // Détection / overwatch
+        public void GainSpottingXP(int enemiesSpotted) =>
+            _skills[SkillType.Perception].AddXP(10 + enemiesSpotted * 6);
+
+        public void GainOverwatchXP(bool hit) =>
+            _skills[SkillType.Perception].AddXP(hit ? 12 : 4);
+
+        // Hack / tech
+        public void GainHackXP(bool success, int difficulty) =>
+            _skills[SkillType.Intelligence].AddXP(success ? 15 + difficulty * 5 : 5);
+
+        // Furtivité
+        public void GainStealthXP(int tilesMovedUndetected) =>
+            _skills[SkillType.Stealth].AddXP(tilesMovedUndetected * 4);
+
+        // Médical
+        public void GainHealingXP(int hpRestored, bool savedLife) =>
+            _skills[SkillType.Medical].AddXP(hpRestored + (savedLife ? 20 : 0));
+
+        // Démolition
+        public void GainDemolitionXP(int coverDestroyed, int totalDamage) =>
+            _skills[SkillType.Demolitions].AddXP(10 + coverDestroyed * 8 + totalDamage / 2);
+
+        // 🎒 Équipement lourd / surcharge
+        public void GainHeavyCarryXP(int weight, int tilesMoved)
+        {
+            _skills[SkillType.Strength]
+                .AddXP((weight / 5) * tilesMoved);
+        }
+
+        // 🔨 Mêlée / corps-à-corps
+        public void GainMeleeXP(int damage, bool kill)
+        {
+            _skills[SkillType.Strength]
+                .AddXP(10 + damage / 2 + (kill ? 15 : 0));
+        }
+
+        // Leadership
+        public void GainLeadershipXP(int alliesNearby, bool squadSuccess)
+        {
+            _skills[SkillType.Leadership]
+                .AddXP(alliesNearby * 5 + (squadSuccess ? 20 : 0));
+        }
+
+        // 🛡️ Resilience (ou Toughness)
+        public void GainResilienceXP(int damageReduced)
+        {
+            _skills[SkillType.Resilience]
+                .AddXP(damageReduced * 3);
+        }
+        public void GainWeaponHandlingXP(int shotsFired, bool movedBeforeShot)
+        {
+            _skills[SkillType.WeaponHandling]
+                .AddXP(shotsFired * (movedBeforeShot ? 6 : 3));
+        }
+
+        // 🧭 Mobility(différent d’Endurance)
+        public void GainMobilityXP(int shortMoves)
+        {
+            _skills[SkillType.Mobility].AddXP(shortMoves * 5);
+        }
+
+
+
         // Bonuses
-        public int GetMovementBonus() => _skills[SkillType.Endurance].Level / 3;
-        public int GetAccuracyBonus() => _skills[SkillType.Marksmanship].Level * 2;
-        public int GetDamageBonus() => _skills[SkillType.Strength].Level;
-        public int GetDefenseBonus() => _skills[SkillType.Tactics].Level * 2;
-        public int GetHealthBonus() => _skills[SkillType.Willpower].Level * 5;
+        public int GetMovementBonus() => 
+            _skills[SkillType.Endurance].Level / 3;
+        public int GetAccuracyBonus() => 
+            _skills[SkillType.Marksmanship].Level * 2;
+        public int GetDefenseBonus() => 
+            _skills[SkillType.Tactics].Level * 2;
+        public int GetHealthBonus() => 
+            _skills[SkillType.Willpower].Level * 5;
+        public int GetVisionBonus() =>
+            _skills[SkillType.Perception].Level / 2;
+        public int GetHackBonus() =>
+            _skills[SkillType.Intelligence].Level * 3;
+        public int GetStealthBonus() =>
+            _skills[SkillType.Stealth].Level * 2;
+        public int GetHealingBonus() =>
+            _skills[SkillType.Medical].Level * 4;
+        public int GetExplosionRadiusBonus() =>
+            _skills[SkillType.Demolitions].Level / 3;
+        public int GetCarryBonus() =>
+            _skills[SkillType.Strength].Level * 2;
+        public int GetMeleeHitBonus() =>
+            _skills[SkillType.Strength].Level * 3;
+        public int GetMoraleBonus() =>
+            _skills[SkillType.Leadership].Level * 2;
+        public int GetRecoilReduction() =>
+            _skills[SkillType.WeaponHandling].Level * 2;
+        public int GetReloadSpeedBonus() =>
+            _skills[SkillType.WeaponHandling].Level * 2;
+
+
 
         public string GetSkillsSummary() =>
             $"Niveau global: {OverallLevel}\n" +
